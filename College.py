b702249832e22d48af0dc878adf8d8e3a76c7cb1
anitha ganesan <anithagk12@gmail.com>
2019-10-20 20:09:58 +0530

    Add files via upload

---
+++ College.py
@@ -0,0 +1,31 @@
+import overpy
+import sys
+import csv
+import overpass
+
+api = overpy.Overpass()
+fobj=open("Colleges.csv","w")
+fobj.write("Colleges in Chennai")
+id="ID"
+name="NAME"
+link ="LINK"
+head=(id+","+link+","+name)
+fobj.write('\n'+head)
+results = api.query("""[out:json][timeout:3600];(node["amenity"="college"](12.9201721,80.1238331,13.2401721,80.4438331)["name:ta"!~"."](12.9201721,80.1238331,13.2401721,80.4438331););out meta;>;out meta qt;
+""")
+for way in results.nodes:
+    EnName=way.tags.get("name:en")       
+    if EnName==None:
+        Name=way.tags.get("name")
+        EnName=Name
+        if EnName==None:
+            EnName="None"
+    Id=way.id
+    id=str(Id)
+    osm="https://www.openstreetmap.org/node/"
+    OSM=str(osm)
+    Link=OSM + id
+    print(EnName)
+    print(id)
+    inp=(id+","+Link+","+EnName)
+    fobj.write('\n'+inp)
---
+++ school.py
@@ -0,0 +1,41 @@
+import overpy
+import sys
+import csv
+import overpass
+
+api = overpy.Overpass()
+fobj=open("schools.csv","w")
+fobj.write("schools in Chennai")
+id="ID"
+name="NAME"
+link ="LINK"
+head=(id+","+link+","+name)
+fobj.write('\n'+head)
+results = api.query("""[out:json][timeout:3600];(node["amenity"="school"](12.9201721,80.1238331,13.2401721,80.4438331)["name:ta"!~"."](12.9201721,80.1238331,13.2401721,80.4438331););out meta;>;out meta qt;
+""")
+for way in results.nodes:
+    
+    Name=way.tags.get("name")
+    if Name==None:
+        EnName=way.tags.get("name:en")
+        if EnName==None: 
+            EnName="None"
+    else:
+        Words=set('aeious')
+        Ename=set(Name)
+        if ((Ename & Words)==set([])):
+            EnName="None"
+        else:
+            EnName=Name
+              
+    
+    Id=way.id
+    id=str(Id)
+    osm="https://www.openstreetmap.org/node/"
+    OSM=str(osm)
+    name= str(EnName)
+    Link=OSM + id
+    print(name)
+    print(id)
+    inp=(id+","+Link+","+name)
+    fobj.write('\n'+inp)
